73a8bcd86d739d09cd1d1955c39d3a7c573e1c53
Lama3L9R <Guzhipei@outlook.com>
2020-03-21 17:52:21 +0800

    添加项目文件。

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+
+namespace PasswordSec {
+    //TO-DO ARRAY {
+    //   TODO Full App
+    //   TODO Socket
+    //   TODO Protect
+    //}
+    
+    public partial class MainWindow : Window {
+
+
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e) {
+
+        }
+
+        public bool checkUpdate()
+        {
+            return true;
+        }
+    }
+}
